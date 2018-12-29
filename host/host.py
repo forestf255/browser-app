80ee7fd8dce9aa6f2cc156e6be521fe179e092ed
Forest Fraser <forestfraser@Forests-MBP.home>
2018-12-29 13:43:12 -1000

    Initial commit

---
+++ host/host.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+sensorData = {
+    "sensor1": 0,
+    "sensor2": 0,
+    "sensor3": 0,
+}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        for k in sensorData.keys():
+            if k in request.form:
+                sensorData[k] = int(request.form[k])
+        return render_template('table.html', **sensorData)
+    else:
+        return render_template('table.html', **sensorData)
